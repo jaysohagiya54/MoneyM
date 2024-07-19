cfe2a0c83f256664139b37946d9548f216c8ff3e
jaysohagiya54 <jaysuhagiya80@gmail.com>
2024-07-19 15:24:58 +0530

    ff

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,7 @@
 import { NextResponse } from "next/server"
-import  prisma from "@repo/db/client";
 
 
 export const GET = async () => {
-    await prisma.user.create({
-        data: {
-            email: "asd",
-            name: "adsads",
-            number:"rtr",
-            password:"tttyt"
-        }
-    })
     return NextResponse.json({
         message: "hi there"
     })
